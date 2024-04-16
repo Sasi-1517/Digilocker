d1d4dd88141313f8e497f8dd181e058e43d5bfbf
unknown <Sasi-1517>
2024-04-16 12:04:18 -0400

    Db changes

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 
 // const URI ='mongodb+srv://bus:bus@cluster0.imsynad.mongodb.net/bus?retryWrites=true&w=majority'
-const URI = "mongodb+srv://abdulsieora:0sjjisQY9J4ketN1@backend.hupih9r.mongodb.net/Digilocker?retryWrites=true&w=majority&appName=Backend"
+// const URI = "mongodb+srv://abdulsieora:0sjjisQY9J4ketN1@backend.hupih9r.mongodb.net/Digilocker?retryWrites=true&w=majority&appName=Backend"
+const URI = "mongodb+srv://lakshmi:Lakshmi%401234@cluster0.te18a1i.mongodb.net/Digilocker"
 
 const connectDB = async () => {
     try {
